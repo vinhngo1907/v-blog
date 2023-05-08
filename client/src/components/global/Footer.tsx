13a3c61099343f4adfc1c31ec06a8c55723a3fbc
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-09 00:08:04 +0700

    continue config source

---
+++ client/src/components/global/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>Welcome my github VN-VietNam</h6>
+      <a href="https://github.com/vinhngo001" 
+      target="_blank" rel="noreferrer"
+      className="mb-2 d-block">
+        https://github.com/vinhngo001
+      </a>
+      <p> Copyright &copy; 2023</p>
+    </div>
+  )
+}
+
+export default Footer
